12799f0015095adcc731df408429574e7144f877
Manabu Matsumoto <mmatsumoto1026@gmail.com>
2021-07-12 20:34:40 +0900

    passwords are encrypted using bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // ストレッチングの回数
+
 app.use(bodyParser.json());
 
 const mockUserData=[
@@ -25,12 +28,12 @@ app.get('/users/:id',function(req,res){
 
 app.post('/login',function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const hashedPassword=bcrypt.hash(req.body.password, saltRounds);
 
     const mockUsername="billyTheKid";
     const mockPassword="superSecret";
 
-    if (username===mockUsername && password===mockPassword) {
+    if (username===mockUsername && bcrypt.compareSync(mockPassword, hashedPassword)) {
         res.json({
             success: true,
             message: 'password and username match!',
